ad4ddb4e32bda6ac6f43f410214be72fd75e2ae8
dullmode <codiwlove@yahoo.co.jp>
2024-12-15 14:51:22 +0900

    fff

---
+++ frontend/src/app/media/press_release/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ArticleList from "~/components/ArticleList"; // Import the PressDetail component
+
+const App = () => {
+  return (
+    <div>
+      <ArticleList />
+    </div>
+  );
+};
+
+export default App;
+
