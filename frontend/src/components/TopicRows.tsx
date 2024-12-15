6c653a0a23e087b98eeb0f91f53c11d269ba7c79
Ocha-Misaki <himawari.158fuji@gmail.com>
2024-12-15 09:53:15 +0900

    topicのrowを作成

---
+++ frontend/src/components/TopicRows.tsx
@@ -0,0 +1,21 @@
+"use client"
+import Topic from "./Topic";
+import { FC } from "react";
+
+// サンプルデータ
+const Topics = [
+  // { title: "編集", companyName: "This is the companyName of Press Release 1.", },
+  // { title: "編集", companyName: "This is the companyName of Press Release 2." },
+];
+
+const TopicRows: FC = () => {
+  return (
+  //   <div className="container mx-auto my-5">
+  //     {Topics.map((release) => (
+  //       <Topic title={release.title} companyName={release.companyName} />
+  //     ))}
+  //   </div>
+  // );
+};
+
+export default TopicRows;
