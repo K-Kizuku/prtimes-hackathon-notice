35d67984a686e6cd8a1abd8e6413443a5f863052
Ocha-Misaki <himawari.158fuji@gmail.com>
2024-12-15 11:05:52 +0900

    フォームのスタイルを修正

--- frontend/src/components/PressLeleaseForm.tsx
+++ frontend/src/components/PressLeleaseForm.tsx
@@ -11,7 +11,7 @@ export const PressLeleaseForm = () => {
       <input className="text-xl p-5 text-black" placeholder="リリースタイトル" type="text" name="title" />
       <input className="p-5 text-black" placeholder="サブタイトル" type="text" name="subTitle" />
       <input className="p-5 text-black" placeholder="画像URL" type="text" name="imgURL" />
-      <input className="p-5 text-black" placeholder="本文" type="textarea" name="description" />
+      <textarea className="px-5 pt-5 pb-40 text-black" placeholder="本文" name="description" />
       <button className="w-32 mt-4 px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2" type="submit">送信</button>
     </form>
   );
--- frontend/src/components/TopicForm.tsx
+++ frontend/src/components/TopicForm.tsx
@@ -8,10 +8,9 @@ export const TopicForm = () => {
   return (
     <form action={dispatch} className="flex flex-col p-5">
       {pending && <p>送信中</p>}
-      <input className="text-xl p-5 text-black" placeholder="リリースタイトル" type="text" name="title" />
-      <input className="p-5 text-black" placeholder="サブタイトル" type="text" name="subTitle" />
-      <input className="p-5 text-black" placeholder="画像URL" type="text" name="imgURL" />
-      <input className="p-5 text-black" placeholder="本文" type="textarea" name="description" />
+      <input className="text-xl p-5 text-black" placeholder="トピックタイトル" type="text" name="title" />
+      <textarea className="px-5 pt-5 pb-40 text-black" placeholder="トピック説明" name="description" />
+      <input className="p-5 text-black" placeholder="トピック画像URL" type="text" name="imgURL" />
       <button className="w-32 mt-4 px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2" type="submit">送信</button>
     </form>
   );
